737428be30899ab9c3c89e3040c201579cd76466
Hao N <peterneo168@gmail.com>
2023-11-04 16:45:49 +0800

    1st commit

---
+++ main.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "hao-s3-sqs"
+  
+}
+
+data "aws_iam_policy_document" "queue" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = ["arn:aws:sqs:*:*:hao1-event-notification-queue"]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.bucket.arn]
+    }
+  }
+}
+
+resource "aws_sqs_queue" "queue" {
+  name   = "hao1-event-notification-queue"
+  policy = data.aws_iam_policy_document.queue.json
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  queue {
+    queue_arn     = aws_sqs_queue.queue.arn
+    events        = ["s3:ObjectCreated:*"]
+
+  }
+}
